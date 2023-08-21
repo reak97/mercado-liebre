5a84a753790979efa1ce07fc9cdb8761a4d1e2ec
reak97 <sebamontero15@gmail.com>
2023-08-21 20:38:24 -0300

    add css to register and login

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 const app = express(); 
 app.use(express.static(path.join(__dirname,"../public")))
-app.listen (3000, ()=>{
+app.listen (3001, ()=>{
     console.log ("Server On");
 })
 app.get("/", (req, res)=>{
